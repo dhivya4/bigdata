3e092b6c93a610229b8b07eb18d2ad94a148f3f9
rj3hadoop <rajbharath.cse@gmail.com>
2017-06-29 04:05:19 +0530

    Update rajwordmapper.java

--- hdpexamples/java/src/com/cloudxlab/Training1/rajwordmapper.java
+++ hdpexamples/java/src/com/cloudxlab/Training1/rajwordmapper.java
@@ -6,22 +6,22 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class rajwordmapper extends Mapper<Object,Text,Text,LongWritable>
+public class rajwordmapper extends Mapper<Object, Text, Text, LongWritable>
 {
-public void mapper(LongWritable key, Text value, Context context)
+public void map(Object key, Text value, Context context)
 {
 
-  String[] words = value.toString().split("[ /t]+")
+  String[] words = value.toString().split("[ /t]+");
   
   for(String word:words)
   {
   
-   word = word.replaceAll("[^a-zA-Z0-9]","")
-   word = word.toLowercase()
+   word = word.replaceAll("[^a-zA-Z0-9]","");
+   word = word.toLowercase();
    
-   Text outkey = new Text(word)
-   LongWritable one = new LongWritable(1)
-   context.write(outkey,one)
+   Text outkey = new Text(word);
+   LongWritable one = new LongWritable(1);
+   context.write(outkey,one);
    
   }
 
