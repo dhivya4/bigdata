67b66f5b1d7aae2d235fb19bbffbb223642c2228
rj3hadoop <rajbharath.cse@gmail.com>
2017-06-29 04:22:43 +0530

    Create rajwordreducer.java

---
+++ hdpexamples/java/src/com/cloudxlab/Training1/rajwordreducer.java
@@ -0,0 +1,26 @@
+package com.cloudxlab.Training1;
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class rajwordreducer extends reducer<Text, LongWritable, Text, LongWritable>
+throws IOException, InterruptedException
+{
+public void reduce(Text key, Iterable <LongWritable> values, Context context)
+{
+Long sum = 0;
+for(LongWritable iw:values)
+{
+ sum = sum + iw
+}
+
+context.write(key,sum)
+
+}
+
+
+}
