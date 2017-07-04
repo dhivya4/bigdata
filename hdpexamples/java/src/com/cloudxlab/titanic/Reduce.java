dd87e80bd82a502809cb12c8a1160bbd28127acd
rj3hadoop <rajbharath.cse@gmail.com>
2017-07-04 17:12:05 +0530

    Update Reduce.java

--- hdpexamples/java/src/com/cloudxlab/titanic/Reduce.java
+++ hdpexamples/java/src/com/cloudxlab/titanic/Reduce.java
@@ -1,3 +1,13 @@
+package com.cloudxlab.titanic;
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
 public static class Reduce extends Reducer<Key_value, IntWritable, Key_value, IntWritable> {
  
 public void reduce(Key_value key, Iterable<IntWritable> values, Context context)
