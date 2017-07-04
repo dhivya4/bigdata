ced0392f794a8bdd1fe40465bca34aa286dd732b
rj3hadoop <rajbharath.cse@gmail.com>
2017-07-04 17:09:15 +0530

    Update Female_survived.java

--- hdpexamples/java/src/com/cloudxlab/titanic/Female_survived.java
+++ hdpexamples/java/src/com/cloudxlab/titanic/Female_survived.java
@@ -1,3 +1,22 @@
+
+package com.cloudxlab.wordcount;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.ArrayWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FixedLengthInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import com.cloudxlab.customreader.NLinesInputFormat;
+
+public class Female_survived {
+
+
 public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
  
 Configuration conf=new Configuration();
@@ -32,4 +51,6 @@ FileOutputFormat.setOutputPath(job, new Path(args[1]));
  
 job.waitForCompletion(true);
  
+}
+
 }
