0720f4d5ed1b7a2985db8aaca71c887f69f11cfc
dhivya4 <dhivya4@gmail.com>
2017-07-09 17:58:32 +0530

    Update rajwordreducer.java

--- hdpexamples/java/src/com/cloudxlab/Training1/rajwordreducer.java
+++ hdpexamples/java/src/com/cloudxlab/Training1/rajwordreducer.java
@@ -8,9 +8,9 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class rajwordreducer extends Reducer<Text, LongWritable, Text, LongWritable>
-throws IOException, InterruptedException
 {
 public void reduce(Text key, Iterable <LongWritable> values, Context context)
+throws IOException, InterruptedException
 {
 Long sum = 0;
 for(LongWritable iw:values)
